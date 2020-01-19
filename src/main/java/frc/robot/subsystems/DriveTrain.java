9024df468c51318e184537b43351280dd0bb1f07
Ultimat0 <marknazzaro2@gmail.com>
2020-01-19 16:01:45 -0500

    added drive train

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -7,15 +7,43 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.kauailabs.navx.frc.AHRS;
 
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.SPI.Port;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.kinematics.DifferentialDriveOdometry;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+@SuppressWarnings("all")
 public class DriveTrain extends SubsystemBase {
   /**
    * Creates a new ExampleSubsystem.
    */
-  public DriveTrain() {
 
+  private final SpeedControllerGroup leftMotors =
+      new SpeedControllerGroup(new WPI_TalonSRX(Constants.MIDDLE_LEFT_TALON_ID),
+                               new WPI_TalonSRX(Constants.BACK_LEFT_TALON_ID));
+
+  private final SpeedControllerGroup rightMotors  =
+      new SpeedControllerGroup(new WPI_TalonSRX(Constants.MIDDLE_RIGHT_TALON_ID),
+                               new WPI_TalonSRX(Constants.BACK_RIGHT_TALON_ID));
+
+  DifferentialDrive robotDrive = new DifferentialDrive(leftMotors, rightMotors);
+
+  AHRS navx = new AHRS(Port.kMXP, (byte) 200);
+
+  DifferentialDriveOdometry odometry;
+
+  public DriveTrain() {
+    odometry = new DifferentialDriveOdometry(Rotation2d.fromDegrees(0));
   }
+
+
 
   @Override
   public void periodic() {
